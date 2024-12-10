7f7be829cc8c4af94525c44f9c21e3bbe3bc85d2
minamichimaa <minamichimaa@gmail.com>
2024-12-09 22:46:57 -0500

    finished day 9

---
+++ Day09/day09p1.py
@@ -0,0 +1,62 @@
+def prettyPrint(array: list):
+    for i in array:
+        print(i.strip())
+
+# 2333133121414131402
+# 0099811188827773336446555566..............
+# 1928
+
+## input
+with open("input.txt", 'r') as f:
+    textIn = f.readlines()
+    
+prettyPrint(textIn)
+
+diskMap = textIn[0].strip()
+
+# initalized blocks
+blocks = []
+currentID = 0
+for i, v in enumerate(diskMap):
+    eveness = i % 2
+    
+    # ID
+    if eveness == 0:
+        for x in range(int(v)):
+            blocks.append(currentID)
+        currentID += 1
+    # space
+    else:
+        for x in range(int(v)):
+            blocks.append('.')
+    
+print(''.join(str(x) for x in blocks))
+
+# reorder
+beginning = 0
+end = len(blocks) - 1
+
+# move pointers until they meet
+while beginning <= end:
+    # found empty space
+    if blocks[beginning] == '.':
+        # move memory
+        blocks[beginning] = blocks[end]
+        blocks[end] = '.'
+
+        # move right pointer
+        while True:
+            end -= 1
+            if blocks[end] != '.':
+                break
+
+    # move left pointer
+    beginning += 1
+
+# calculate result
+total = 0
+for i, v in enumerate(blocks):
+    if v != '.':
+        total += i * v
+
+print(total)
---
+++ Day09/day09p2.py
@@ -0,0 +1,90 @@
+def prettyPrint(array: list):
+    for i in array:
+        print(i.strip())
+
+# 2333133121414131402
+# 00...111...2...333.44.5555.6666.777.888899
+# 00992111777.44.33388885555.6666...........
+# 1928
+
+# 00...111...2...333.44.5555.6666.777.888899
+# 0099.111...2...333.44.5555.6666.777.8888..
+# 0099.1117772...333.44.5555.6666.....8888..
+# 0099.111777244.333....5555.6666.....8888..
+# 00992111777.44.333....5555.6666.....8888..
+
+## input
+with open("input.txt", 'r') as f:
+    textIn = f.readlines()
+    
+prettyPrint(textIn)
+
+diskMap = textIn[0].strip()
+
+# initalized blocks
+blocks = []
+currentID = 0
+ids = {}
+for i, v in enumerate(diskMap):
+    eveness = i % 2
+
+    # ID
+    if eveness == 0:
+        # write memory
+        currentIndex = len(blocks)
+        for x in range(int(v)):
+            blocks.append(currentID)
+        
+        # save in dictionary
+        blockObj = {'startingIndex': currentIndex, 'length': int(v)}
+        ids[currentID] = blockObj
+        # increment id for next
+        currentID += 1
+
+    # space
+    else:
+        # write space
+        for x in range(int(v)):
+            blocks.append('.')
+
+# reorder
+for id in reversed(ids):
+    # initialize
+    startingSpace = None
+    currentLengthSpace = 0
+    # iterate through blocks
+    for i, v in enumerate(blocks):
+        # if reached starting postion of id, reset
+        if i == ids[id]['startingIndex']:
+            startingSpace = None
+            currentLengthSpace = 0
+            break
+        # save position and current length of space
+        if v == '.':
+            if startingSpace == None:
+                startingSpace = i
+            currentLengthSpace += 1
+        # otherwise reset position
+        else:
+            startingSpace = None
+            currentLengthSpace = 0
+        # if length is long enough, move
+        if currentLengthSpace == ids[id]['length']:
+            # write new memory
+            for x in range(startingSpace, startingSpace+currentLengthSpace):
+                blocks[x] = id
+            # free memory
+            for x in range(ids[id]['startingIndex'], ids[id]['startingIndex']+ ids[id]['length']):
+                blocks[x] = '.'
+            # reset space
+            startingSpace = None
+            currentLengthSpace = 0
+            break
+    
+# calculate result
+total = 0
+for i, v in enumerate(blocks):
+    if v != '.':
+        total += i * v
+
+print(total)
