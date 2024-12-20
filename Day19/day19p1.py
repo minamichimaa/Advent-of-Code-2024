793eba639e532c2ad5591d8b1d67c1e0142328b0
minamichimaa <minamichimaa@gmail.com>
2024-12-19 21:24:44 -0500

    finished day19

---
+++ Day19/day19p1.py
@@ -0,0 +1,41 @@
+def prettyPrint(array: list[str]):
+    for i in array:
+        print(i.strip())
+        
+myCache = {}
+patterns = tuple()
+def isValidPattern(design) -> bool:
+    # previously found
+    if design in myCache:
+        return myCache[design]
+    
+    # split substrings
+    for i in range(1, len(design)):
+        part1 = design[:i]
+        part2 = design[i:]
+
+        # recursively check substrings
+        if part1 in patterns and (part2 in patterns or isValidPattern(part2)):
+            # found # cache and return
+            myCache[design] = True
+            return True
+        
+    # no valid pattern # cache and return
+    myCache[design] = False
+    return False
+
+## input
+with open("input.txt", 'r') as f:
+    textIn = f.readlines()
+
+# parse input
+patterns = tuple([x for x in textIn[0].strip().split(', ')])
+designs = tuple([x.strip() for x in textIn[2:]])
+
+# could valid designs
+valid = 0
+for design in designs:
+    if isValidPattern(design):
+        valid += 1
+     
+print(valid)
---
+++ Day19/day19p2.py
@@ -0,0 +1,46 @@
+def prettyPrint(array: list[str]):
+    for i in array:
+        print(i.strip())
+        
+myCache = {}
+patterns = tuple()
+def isValidPattern(design) -> list:
+    # previously found
+    if design in myCache:
+        return myCache[design]
+    
+    # new
+    validArrangements = 0
+    
+    # split substrings
+    for i in range(1, len(design)):
+        part1 = design[:i]
+        part2 = design[i:]
+
+        # recursively check substrings
+        if part1 in patterns:
+            # if exists in patterns
+            if part2 in patterns:
+                validArrangements += 1
+            # find num of patterns from the substring
+            validArrangements += isValidPattern(part2)
+        
+    # cache and return
+    myCache[design] = validArrangements
+    return validArrangements
+
+
+## input
+with open("input.txt", 'r') as f:
+    textIn = f.readlines()
+    
+# parse input
+patterns = tuple([x for x in textIn[0].strip().split(', ')])
+designs = tuple([x.strip() for x in textIn[2:]])
+
+# count variations of designs
+valid = 0
+for design in designs:
+    valid += isValidPattern(design)
+     
+print(valid)
