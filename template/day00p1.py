f766100eefe3b5f4db6bf8fabdcd3907c1c13ab8
minamichimaa <minamichimaa@gmail.com>
2025-02-13 10:46:27 -0500

    Refactored with Black formatting

--- template/day00p1.py
+++ template/day00p1.py
@@ -2,8 +2,9 @@ def prettyPrint(array: list[str]):
     for i in array:
         print(i.strip())
 
+
 ## input
-with open("test.txt", 'r') as f:
+with open("test.txt", "r") as f:
     textIn = f.readlines()
-    
+
 prettyPrint(textIn)
